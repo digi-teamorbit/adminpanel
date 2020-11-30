5110171da9c13e35660e0760d2fd0cd0644b0b6a
Team Orbit <68843851+digi-teamorbit@users.noreply.github.com>
2020-11-30 11:31:16 +0500

    Update LoggedInController.php

--- app/Http/Controllers/LoggedInController.php
+++ app/Http/Controllers/LoggedInController.php
@@ -150,18 +150,20 @@ class LoggedInController extends Controller
 
 	}
 
-	public function updateAccount(Request $request) {	
-
+public function updateAccount(Request $request) {	
 
 		$user = DB::table('users')->where('id', Auth::user()->id)->first();
 		
 		$insertArr['name'] = $_POST['name'];	
 		$insertArr['email'] = $_POST['email'];
-
+		
+		
+		$password = $_POST['password'];
+		$confirmpass = $_POST['password_confirmation'];
+		  if($password == $confirmpass ){
 		if(trim($_POST['password']) != "") {
 				$insertArr['password'] = Hash::make($_POST['password']);
-		}
-
+		} 
 		DB::table('users')
 		->where('id', Auth::user()->id)
 		->update(
@@ -171,7 +173,15 @@ class LoggedInController extends Controller
 		Session::flash('message', 'Your password settings has been changed'); 
 		Session::flash('alert-class', 'alert-success'); 
 		return back();			
-		
+		  }
+		  else{
+			  
+	    Session::flash('flash_message', 'Password do not match'); 
+		Session::flash('alert-class', 'alert-danger'); 
+		return back();	
+			  
+		  }
+			  
 	}
 
 
